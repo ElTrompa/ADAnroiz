abd016e8581915aa0459c8b15a5f0fe6da14e1d7
ElTrompa <159024200+Trompa06@users.noreply.github.com>
2025-10-03 18:48:03 +0200

    cargar horarios

--- untitled/src/GestorSustituciones.java
+++ untitled/src/GestorSustituciones.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class GestorSustituciones {
@@ -8,5 +11,30 @@ public class GestorSustituciones {
         this.profesores = profesores;
     }
 
+    public void cargarHorarios(String ficheroHorario) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(ficheroHorario))) {
+            String linea;
+            while((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) continue;
+                String[] datos = linea.split(",");
+                if (datos.length < 4) continue;
 
+                String nombre = datos[0];
+                String dia = datos[1];
+                int hora = Integer.parseInt(datos[2]);
+                String clase = datos[3];
+
+                Profesor pr = new Profesor(nombre, hora, clase, dia);
+                profesores.add(pr);
+            }
+        }
+    }
+
+    public String sustituirProfesor(String nombre, int hora, String dia) {
+        String profesorSustitudor = "";
+
+
+
+        return profesorSustitudor;
+    }
 }
