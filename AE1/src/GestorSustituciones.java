20e059c239e31be27fb25277fd23c2a1673a0401
ElTrompa <159024200+Trompa06@users.noreply.github.com>
2025-10-04 17:42:30 +0200

    sustituirProfesor

--- AE1/src/GestorSustituciones.java
+++ AE1/src/GestorSustituciones.java
@@ -3,8 +3,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GestorSustituciones {
+    Scanner teclado = new Scanner(System.in);
+
     private ArrayList<Profesor> profesores = new ArrayList<>();
 
     public GestorSustituciones(ArrayList<Profesor> profesores) {
@@ -33,7 +36,25 @@ public class GestorSustituciones {
     public String sustituirProfesor(String nombre, int hora, String dia) {
         String profesorSustitudor = "";
 
+        System.out.println("PROFESORES DISPONIBLE:");
 
+        for(int i = 0; i < profesores.size(); i++) {
+            if(!nombre.equals(profesores.get(i).getNombre()) && hora == profesores.get(i).getHora() && dia.equals(profesores.get(i).getHora()) && profesores.get(i).getClase().equals("Libre")) {
+                System.out.println("- " + profesores.get(i).getNombre());
+            }
+        }
+
+        System.out.println("¿Que profesor quieres elegir?");
+        profesorSustitudor = teclado.next();
+
+        for(int i = 0; i < profesores.size(); i++) {
+            if (profesorSustitudor.equals(profesores.get(i).getNombre()) && hora == profesores.get(i).getHora() && dia.equals(profesores.get(i).getHora()) && profesores.get(i).getClase().equals("Libre")) {
+                System.out.println("Profesor elegido para sustituir a " + nombre + " es " + profesorSustitudor);
+            } else {
+                System.out.println("Algo a salido mal");
+                profesorSustitudor = "ERROR";
+            }
+        }
 
         return profesorSustitudor;
     }
