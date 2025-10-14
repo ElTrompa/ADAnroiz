6aa52627d16439e48c751834ff2a4d97511b668b
ElTrompa <159024200+Trompa06@users.noreply.github.com>
2025-10-14 11:09:28 +0200

    update

--- GestorFicheros/src/GestorFicheros.java
+++ GestorFicheros/src/GestorFicheros.java
@@ -1,6 +1,20 @@
-public class GestorFicheros {
-    public void getInformacio () {
+import java.io.File;
+import java.util.Date;
 
+public class GestorFicheros {
+    public String getInformacio (String ruta) {
+        File archivo = new File(ruta);
+
+        String nombre = "";
+        String tipo = "";
+        String ubicacio = "";
+        Date dataUltimaModificacio = ;
+        boolean ocult = false;
+
+
+
+
+        return nombre, tipo, ubicacio, dataUltimaModificacio, ocult;
     }
 
     public void creaCarpeta () {
