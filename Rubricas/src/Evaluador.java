8a0df368d3e04c1410a3af98d3b5a7d9e1304803
Trompa06 <159024200+Trompa06@users.noreply.github.com>
2025-10-20 16:33:54 +0200

    update

--- Rubricas/src/Evaluador.java
+++ Rubricas/src/Evaluador.java
@@ -1,3 +1,44 @@
-public class Evaluador {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Evaluador {
+    public ArrayList<String> leerNombres(String ruta) throws IOException {
+        ArrayList<String> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                if(linea.trim().isEmpty()) continue;
+                String[] partes = linea.split(";");
+                lista.add(Arrays.toString(partes));
+            }
+        }
+
+        return lista;
+    }
+
+    public ArrayList<String> leerRubica(String ruta) throws IOException {
+        ArrayList<String> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            boolean primera = true;
+
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) continue;
+                if (primera) { // saltar cabecera
+                    primera = false;
+                    continue;
+                }
+
+                String[] partes = linea.split(";");
+                lista.add(Arrays.toString(partes));
+            }
+        }
+
+        return lista;
+    }
 }
--- Rubricas/src/Main.java
+++ Rubricas/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
         System.out.println("Rubricas:");
     }
 }
