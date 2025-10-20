551720d4933b56adfa42a6a7ee4b951629b14cad
Trompa06 <159024200+Trompa06@users.noreply.github.com>
2025-10-20 17:38:52 +0200

    update

--- Rubricas/src/Main.java
+++ Rubricas/src/Main.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        Evaluador evaluador = new Evaluador();
 
-        System.out.println("Rubricas:");
+        String nombresFile = "nombres.txt";
+        String rubricaFile = "rubrica.txt";
+
+        try {
+            ArrayList<String> alumnos = evaluador.leerNombres(nombresFile);
+
+            System.out.println("Alumnos:");
+            for (int i = 0; i < alumnos.size(); i++) {
+                String a = alumnos.get(i).replace("[", "").replace("]", "");
+                System.out.printf("%d. %s%n", i + 1, a);
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
