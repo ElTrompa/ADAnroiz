306774aa981d11647ca3167b9d74e93304aa6bc5
Trompa06 <159024200+Trompa06@users.noreply.github.com>
2025-10-29 20:09:57 +0100

    update

--- AE3/src/Main.java
+++ AE3/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de gestion de faltas de alumnos");
+        System.out.println("Sistema de gestion de faltas de alumnos ");
         Gestor gestor = new Gestor();
         gestor.menu();
     }
