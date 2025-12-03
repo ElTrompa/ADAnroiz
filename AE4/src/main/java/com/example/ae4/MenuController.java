16f609a82294729f8c0ac18caa42617349378d78
ElTrompa <anroiz@alumnatflorida.es>
2025-12-03 23:09:38 +0100

    menu fxml

--- AE4/src/main/java/com/example/ae4/MenuController.java
+++ AE4/src/main/java/com/example/ae4/MenuController.java
@@ -1,5 +1,41 @@
 package com.example.ae4;
 
-public class MenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.hibernate.Hibernate;
 
+public class MenuController {
+    @FXML
+    private Button ficharButton;
+
+    @FXML
+    private Button buscarEmpleadoButton;
+
+    @FXML
+    private Button estadisticasButton;
+
+    @FXML
+    private Button salirButton;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void onFicharButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBuscarEmpleadoButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onSalirButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onEstadisticasButtonClick(ActionEvent actionEvent) {
+    }
 }
